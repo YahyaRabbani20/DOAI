f2a5ad8951c0b71d36433456cea7b6e795397f9d
Yahya Rabbani <141623729+YahyaRabbani20@users.noreply.github.com>
2026-04-24 10:52:25 -0500

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.querySelectorAll('a[href^="#"]').forEach(link => {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: "smooth" });
+    }
   });
 });
